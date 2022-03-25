95bbba58936b3b380e8a2a5ae978436ac7c589f5
Yuta Miyagi <e155721+github@ie.u-ryukyu.ac.jp>
2022-03-26 06:46:23 +0900

    added guessinggame.sh

---
+++ guessinggame.sh
@@ -0,0 +1,34 @@
+function get_current_file_num {
+    local file_num=$(ls -l | grep -v ^- | wc -l)
+    echo $file_num
+}
+
+function judge_user_input {
+
+    local user_input=$1
+    local file_num=$2
+
+    if [[ $user_input -lt $file_num ]]
+    then
+        echo "Your input number is too low." >&2
+        echo 1
+    elif [[ $user_input -gt $file_num ]]
+    then
+        echo "Your input number is too high." >&2
+        echo 1
+    else
+        echo "Congratulation!!" >&2
+        echo 0
+    fi
+}
+
+echo "How many files in the current directory?"
+
+file_num=$(get_current_file_num)
+
+status=1
+while [[ $status -eq 1 ]]
+do
+    read user_input
+    status=$(judge_user_input $user_input $file_num)
+done
